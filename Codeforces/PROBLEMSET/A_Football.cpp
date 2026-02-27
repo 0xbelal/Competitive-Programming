635c9f602f5116d2a4461675017da7d935ba0acb
Belal Elgeaidi <0xbelalmhmd@gmail.com>
2026-02-27 23:22:37 +0200

    update

---
+++ Codeforces/PROBLEMSET/A_Football.cpp
@@ -0,0 +1,83 @@
+/* Deliberate practice > blind repetition */
+// Author: Belal
+// URL: https://codeforces.com/contest/43/problem/A
+
+
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<assert.h>
+
+using namespace std;
+
+
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+const double EPS = (1e-7);
+
+
+void solve() {
+    int n;cin>>n;
+    if(n==1){
+        string x;cin>>x;
+        cout<<x;
+    }
+    map<string,int> freq;
+    for(int i=0;i<n;i++){
+        string x;cin>>x;
+        freq[x]++;
+    }
+    pair<string,int> team;
+    int cnt=1;
+    for(auto &[t,f] : freq){
+        if(cnt==1){team = make_pair(t,f) ; cnt++;}
+        else {
+            if(f > team.second){
+                team = make_pair(t,f);
+            }
+        }
+    }  
+    cout<<team.first;
+}
+
+int main() {
+    fast_io;
+
+    int t = 1;
+
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
